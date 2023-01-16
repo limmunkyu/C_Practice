0699fc3acf5387ef160f4093274feccccca67fdb
임문규 <82082449+limmunkyu@users.noreply.github.com>
2023-01-17 08:48:35 +0900

    Create practice033.c

---
+++ practice033.c
@@ -0,0 +1,9 @@
+//33. 10진 정수 1개 입력받아 16진수로 출력하기
+
+#include<stdio.h>
+int main(void){
+    int a;
+    scanf("%d", &a);  //%x, %X : 부호 없는 정수를 16진수(hexadecimal)로 출력
+    printf("%X", a);  //x는 출력 시 알파벳 부분이 소문자로 X는 대문자로 출력
+    return 0;
+}
