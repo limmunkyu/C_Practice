898fe01f645a9d132a2a4274180c42d64971dafe
임문규 <82082449+limmunkyu@users.noreply.github.com>
2023-01-27 17:46:04 +0900

    Create practice086.c

---
+++ practice086.c
@@ -0,0 +1,11 @@
+//86. 그림 파일 저장용량 계산하기
+
+#include <stdio.h>
+int main() {
+	int w, h, b;
+	double mb;
+	scanf("%d %d %d", &w, &h, &b);
+	mb = (float)w*h*b/8/1024/1024;
+	printf("%.2lf MB", mb);
+	return 0;
+}
